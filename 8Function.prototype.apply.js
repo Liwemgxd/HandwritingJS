1dacd0c5e93c61fe28c93dffbaf9f94bb545fee9
liwemg <wemgli@qq.com>
2021-07-07 17:51:11 +0800

    7-7-1

--- 8Function.prototype.apply.js
+++ 8Function.prototype.apply.js
@@ -11,3 +11,15 @@ Function.prototype.apply_ = function (context = window, args){
     return res;
 }
 
+Function.prototype.apply__ = function (obj, arr){
+    obj = obj? Object(obj):window;
+    obj.fn = this;  //this 就是这个函数
+    let res;
+    if (!arr){
+        res = obj.fn();
+    }else {
+        res = obj.fn(...arr);
+    }
+    delete obj.fn;
+    return res;
+}
